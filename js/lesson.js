e7e59dfd50ccf22f1280752f2af97f2079f06f98
Burlucky <droon200493@gmail.com>
2019-07-16 23:12:11 +0300

    background

---
+++ js/lesson.js
@@ -0,0 +1,11 @@
+  $( document ).ready(function() {
+    scaleVideoContainer();
+
+	$('.first_line').show(3000);
+	$('.second_line').show(6000);
+
+	$('#knopka').on('click',function(){
+		alert('это только начало');
+	})
+
+});
